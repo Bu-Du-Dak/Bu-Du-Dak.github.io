13ca53a67902d38eb3f8ceea4fbb99748a0c18e8
Hong JaeHoon <guri930219@gmail.com>
2022-10-17 00:15:51 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/2E-zlYFDrZhYEFV7GsJV3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-1b181e6f73366e5e.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
