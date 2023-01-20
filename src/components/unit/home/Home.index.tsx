38ce8040ca04988d7a696b028c2bda9cb721c9f7
Bu-Du-Dak <guri930219@gmail.com>
2023-01-20 17:07:19 +0900

    hotfix

--- src/components/unit/home/Home.index.tsx
+++ src/components/unit/home/Home.index.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <>
       <SectionPage title="About Me" Component={<AboutMePage/>}/>
-      <SectionPage title="Experience" Component={<div>aaaa</div>}/>
+      <SectionPage title="Work Experience" Component={<div>드래그 했을경우 컬러 변경, top sticky color line</div>}/>
     </>
   )
 }
