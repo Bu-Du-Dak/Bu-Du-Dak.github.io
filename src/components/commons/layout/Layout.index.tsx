e4cc52d923ae9740ea21518acd43e5c0306315d6
Hong JaeHoon <guri930219@gmail.com>
2023-01-25 23:00:09 +0900

    top line box add .

--- src/components/commons/layout/Layout.index.tsx
+++ src/components/commons/layout/Layout.index.tsx
@@ -4,15 +4,26 @@ import LayoutHeader from "./header/LayoutHeader"
 
 const Layout = (props) => {
   return(
+    <>
+    <TopLine/>
     <Container>
-
       <LayoutWrapper>
         <LayoutHeader/>
         <Body className="container">{props.children}</Body>
       </LayoutWrapper>
     </Container>
+    </>
   )
 }
+const TopLine = styled.div`
+  position: fixed;
+  top: 0;
+  z-index: 1;
+  width: 100%;
+  height: 15px;
+  background: linear-gradient(90deg,#02AAB0,#C4E0E5);
+  /* background: linear-gradient(90deg,#02AAB0,#00CDAC); */
+`
 const LayoutWrapper = styled.div`
   width: 100%;
   display: flex;
@@ -28,7 +39,5 @@ const Body = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  /* background-color: gray; */
-  
 `;
 export default Layout
