3341112d338febe8bff5c27865401c86bfbe3adf
Hong JaeHoon <guri930219@gmail.com>
2023-01-25 23:14:48 +0900

    Revert "hotfix"
    This reverts commit 38ce8040ca04988d7a696b028c2bda9cb721c9f7.

--- src/components/unit/home/Home.index.tsx
+++ src/components/unit/home/Home.index.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <>
       <SectionPage title="About Me" Component={<AboutMePage/>}/>
-      <SectionPage title="Work Experience" Component={<div>드래그 했을경우 컬러 변경, top sticky color line</div>}/>
+      <SectionPage title="Experience" Component={<div>aaaa</div>}/>
     </>
   )
 }
