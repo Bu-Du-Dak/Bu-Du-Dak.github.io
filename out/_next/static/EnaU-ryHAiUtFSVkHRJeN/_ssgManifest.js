1dc47ffe371767be1f1623bbc30aad853a7a0a05
Bu-Du-Dak <guri930219@gmail.com>
2024-06-12 15:31:13 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/EnaU-ryHAiUtFSVkHRJeN/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[category]","\u002Fblog\u002F[category]\u002Fdetail\u002F[id]","\u002Fworks\u002F[year]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
